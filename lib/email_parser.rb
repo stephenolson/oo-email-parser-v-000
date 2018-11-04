faef5209139acf2096291893aa263b564892a4da
Stephen Olson <stephenolson@gmail.com>
2018-11-04 16:30:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,6 +11,12 @@ class EmailParser
     @csv_emails = csv_emails
   end
   
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end
   
   
 end
